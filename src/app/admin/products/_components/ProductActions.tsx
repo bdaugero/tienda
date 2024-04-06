ed3336bd605e9876907c9a5f245c4c86074d7af9
Bautista <bdaugero@gmail.com>
2024-04-06 03:15:28 -0300

    Mas o menos 1 hora del video

---
+++ src/app/admin/products/_components/ProductActions.tsx
@@ -0,0 +1,15 @@
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+
+export function ActiveToggleDropdownItem({
+    id,
+    isAvailableForPurchase,
+}: {
+    id: string, 
+    isAvailableForPurchase: boolean
+}) {
+     <DropdownMenuItem></DropdownMenuItem>
+}
+
+export function DeleteDropdownItem() {
+
+}
--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,7 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { PageHeader } from "../_components/PageHeader";
 import Link from "next/link";
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import db from "@/db/bd";
+import { CheckCircle, CheckCircle2, MoreVertical, XCircle } from "lucide-react";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 export default function AdminProductsPage(){
     return (
@@ -17,20 +21,76 @@ export default function AdminProductsPage(){
     )
 }
 
-function ProductsTable() {
-    return <Table>
-        <TableHeader>
-            <TableRow>
-                <TableHead className="w-0">
-                    <span className="sr-only">Disponible para Comprar</span>
-                </TableHead>
-                <TableHead>Nombre</TableHead>
-                <TableHead>Precio</TableHead>
-                <TableHead>Ordenes</TableHead>
-                <TableHead className="w-0">
-                    <span className="sr-only">Actions</span>
-                </TableHead>
-            </TableRow>
-        </TableHeader>
-    </Table>
+async function ProductsTable() {
+    const products = await db.product.findMany({
+        select: {
+            id: true,
+            name: true,
+            priceInCents: true,
+            isAvailableForPurchase: true,
+            _count: { select: { orders: true }}
+        },
+        orderBy: { name: "asc"}
+    })
+
+    if (products.length === 0) return <p>Todavia no tienes productos</p>
+
+    return (
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-0">
+                            <span className="sr-only">Disponible para Comprar</span>
+                        </TableHead>
+                        <TableHead>Nombre</TableHead>
+                        <TableHead>Precio</TableHead>
+                        <TableHead>Ordenes</TableHead>
+                        <TableHead className="w-0">
+                            <span className="sr-only">Actions</span>
+                        </TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {products.map(product => (
+                        <TableRow key={product.id}>
+                            <TableCell>
+                                {product.isAvailableForPurchase ?( 
+                                    <>
+                                        <span className="sr-only">Producto Disponible</span>
+                                        <CheckCircle2 /> 
+                                    </>
+                                ) : (
+                                    <>
+                                        <span className="sr-only">Producto No Disponible</span>
+                                        <XCircle />
+                                    </>
+                                )}
+                            </TableCell>
+                            <TableCell>{product.name}</TableCell>
+                            <TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+                            <TableCell>{formatNumber(product._count.orders)}</TableCell>
+                            <TableCell>
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger>
+                                        <MoreVertical />
+                                        <span className="sr-only">Ver Mas</span>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent>
+                                        <DropdownMenuItem asChild>
+                                            <a download href={`/admin/products/${product.id}/download`}>Descargar</a>
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem asChild>
+                                            <Link href={`/admin/products/${product.id}/edit`}>
+                                                Editar
+                                            </Link>
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+         </Table>
+    )
 }
+            
